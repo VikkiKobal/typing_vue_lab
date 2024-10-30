16e551215510e2f0eb92e2442590d181e15da0f4
Viktoriia Kobal <kobal.viktoriia@student.uzhnu.edu.ua>
2024-10-30 22:06:51 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="app-container">
+        <h1>Форма оформлення їжі</h1>
+
+        <custom-input v-model:dishName="dishName" v-model:quantity="quantity" v-model:size="size" />
+
+        <div class="order-details">
+            <div>
+                <span>Назва страви: </span><strong>{{ dishName }}</strong>
+            </div>
+            <div>
+                <span>Кількість порцій: </span><strong>{{ quantity }}</strong>
+            </div>
+            <div>
+                <span>Розмір порції: </span><strong>{{ size }}</strong>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import CustomInput from './components/CustomInput.vue'
+
+export default {
+    components: {
+        CustomInput,
+    },
+    data() {
+        return {
+            dishName: '',
+            quantity: 0,
+            size: 'M', // Початкове значення розміру
+        }
+    },
+}
+</script>
+
+<style scoped>
+div {
+    max-width: 400px;
+    margin: 20px auto;
+}
+</style>
---
+++ src/components/CustomInput.vue
@@ -0,0 +1,79 @@
+<template>
+    <div>
+        <label>
+            Введіть назву страви:
+            <input v-model="dishNameInput" placeholder="Назва страви" />
+        </label>
+
+        <label>
+            Введіть кількість порцій:
+            <input type="number" v-model="quantityInput" placeholder="Кількість порцій" />
+        </label>
+
+        <label>
+            Виберіть розмір порції:
+            <select v-model="sizeInput">
+                <option value="S">S</option>
+                <option value="M">M</option>
+                <option value="L">L</option>
+            </select>
+        </label>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'CustomInput',
+    props: {
+        dishName: {
+            type: String,
+            default: '',
+        },
+        quantity: {
+            type: Number,
+            default: 0,
+        },
+        size: {
+            type: String,
+            default: 'M', // За замовчуванням значення M
+        },
+    },
+    computed: {
+        dishNameInput: {
+            get() {
+                return this.dishName
+            },
+            set(newValue) {
+                this.$emit('update:dishName', newValue)
+            },
+        },
+        quantityInput: {
+            get() {
+                return this.quantity
+            },
+            set(newValue) {
+                this.$emit('update:quantity', Number(newValue))
+            },
+        },
+        sizeInput: {
+            get() {
+                return this.size
+            },
+            set(newValue) {
+                this.$emit('update:size', newValue)
+            },
+        },
+    },
+}
+</script>
+
+<style lang="css" scoped>
+label {
+    display: block;
+    margin-bottom: 10px;
+}
+input,
+select {
+    margin-left: 10px;
+}
+</style>
